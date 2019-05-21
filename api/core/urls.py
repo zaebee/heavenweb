e8922fcb033ec3456dcdbc35bd3c602b8e40e0ad
zaebee <zaebuntu@gmail.com>
2019-05-21 19:36:37 +0300

    added migration

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,13 +1,15 @@
 # core/urls.py
-from django.urls import path, include, url
+from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework.routers import DefaultRouter
+
 from .views import RecipeViewSet
+
 
 router = DefaultRouter()
 router.register(r'recipes', RecipeViewSet)
 
 urlpatterns = [
-    path("^api/", include(router.urls)),
-    url(r'^api-token-auth/', obtain_jwt_token),
+    path(r'^api/', include(router.urls)),
+    path(r'^api-token-auth/', obtain_jwt_token),
 ]
