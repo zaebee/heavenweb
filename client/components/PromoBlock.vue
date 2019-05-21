054bb1ccb890f260e870e879f93742ad607a490e
zaebee <zaebuntu@gmail.com>
2019-05-21 23:58:41 +0300

    added promo block

---
+++ client/components/PromoBlock.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="mx-3 mx-md-0" :class="classList">
+      <h1>{{h1}}</h1>
+      <p class="mt-3" v-html="slogan"></p>
+      <nuxt-link
+        class="btn btn-lg btn-info btn-header text-center text-md-left mt-4"
+        to="/chat-bot">
+        {{cta}}&rarr;
+      </nuxt-link>
+    </div>
+    <transition name="page">
+      <subscribe-form
+        v-if="visibleForm"
+        :large="large"
+        :btn-go="btnGo"
+        :close-btn="true"
+        :class="{'open': visibleForm}"
+        @hide-lead="$emit('hide-lead')"
+        ></subscribe-form>
+    </transition>
+  </div>
+</template>
+
+<script>
+  import SubscribeForm from '~/components/SubscribeForm.vue'
+  export default {
+    props: {
+      large: {
+        default: true,
+        type: Boolean,
+      },
+      visibleForm: {
+        default: true,
+        type: Boolean
+      },
+      showPromo: {
+        default: true,
+        type: Boolean
+      },
+      h1: {
+        default: 'Создание и продвижение сайтов',
+        type: String
+      },
+      slogan: {
+        default: 'Приведем клиентов для вашего бизнеса',
+        type: String
+      },
+      cta: {
+        default: 'Заполнить бриф',
+        type: String
+      },
+      btnGo: {
+        default: 'Хочу',
+        type: String
+      },
+    },
+    components: {
+      SubscribeForm,
+    },
+    computed: {
+      classList () {
+        return {
+          'text-box': this.showPromo,
+          'jumbotron': !this.showPromo && !this.large
+        }
+      }
+    },
+    methods: {
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+</style>
